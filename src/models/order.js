5364d2ce34e11382a28c9c17f5b9f0cf631cccd2
Bassit owolabi <barseetbrown@gmail.com>
2022-07-07 07:57:26 +0100

    feat: checkout

--- src/models/order.js
+++ src/models/order.js
@@ -8,7 +8,7 @@ const orderSchema = new mongoose.Schema({
         ref: 'User'
     },
     items: [{
-        productId: {
+        itemId: {
             type: ObjectID,
             ref: 'Item',
             required: true
--- src/routers/order.js
+++ src/routers/order.js
@@ -1,12 +1,14 @@
 const express = require("express")
+const Flutterwave = require("flutterwave-node-v3")
 const Order = require("../models/order")
 const Cart = require("../models/cart")
 const User = require("../models/user")
 const Auth = require("../middleware/auth")
-const stripe = require('stripe')('sk_test_51K5oDhFyMddIu9oDIxWr4FhcaSElSU6IYZXfS04HH4YWz80STi6uJxe7lKrcThMKO3ZKysEVXbcSnuqJpkqRe4QW00ldfUU1v5');
+
 
 const router = new express.Router()
 
+const flw = new Flutterwave(process.env.FLUTTERWAVE_V3_PUBLIC_KEY, process.env.FLUTTERWAVE_V3_SECRET_KEY)
 //get orders
 
 router.get('/orders', Auth, async (req, res) => {
@@ -20,5 +22,64 @@ router.get('/orders', Auth, async (req, res) => {
 })
 
 //checkout
+router.post('/order/checkout', Auth, async(req, res) => {
+    try {
+        const owner = req.user._id;
+        let payload = req.body
+        
+
+        //find cart and user 
+        let cart = await Cart.findOne({owner})
+        let user = await User.findOne({_id: owner})
+        if(cart) {
+            
+        payload = {...payload, amount: cart.bill, email: user.email}
+            const response = await flw.Charge.card(payload)
+           // console.log(response)
+            if(response.meta.authorization.mode === 'pin') {
+                let payload2 = payload
+                payload2.authorization = {
+                    "mode": "pin",
+                    "fields": [
+                        "pin"
+                    ],
+                    "pin": 3310
+                }
+                const reCallCharge = await flw.Charge.card(payload2)
+
+                const callValidate = await flw.Charge.validate({
+                    "otp": "12345",
+                    "flw_ref": reCallCharge.data.flw_ref
+                })
+                console.log(callValidate)
+                if(callValidate.status === 'success') {
+                    const order = await Order.create({
+                        owner,
+                        items: cart.items,
+                        bill: cart.bill
+                    })
+                    //delete cart
+                    const data = await Cart.findByIdAndDelete({_id: cart.id})
+                    return res.status(201).send({status: 'Payment successful', order})
+                } else {
+                    res.status(400).send('payment failed')
+                }
+            }
+            if( response.meta.authorization.mode === 'redirect') {
+
+                let url = response.meta.authorization.redirect
+                open(url)
+            }
+
+           // console.log(response)
+
+        } else {
+            res.status(400).send('No cart found')
+        }
+    } catch (error) {
+        console.log(error)
+        res.status(400).send('invalid request')
+    }
+})
 
 module.exports = router
